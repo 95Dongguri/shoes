51e2e09f5136d0f3a40323b23191280c922107fa
95Dongguri <hyuk_ey@naver.com>
2022-07-14 22:42:03 +0900

    StoryBoard로 전환

---
+++ shoes/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  shoes
+//
+//  Created by 김동혁 on 2022/07/14.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var CurrentStatusView: UIView!
+    @IBOutlet weak var ShoesImageView: UIImageView!
+    @IBOutlet weak var LavelLabel: UILabel!
+    @IBOutlet weak var ShoesNameLabel: UILabel!
+    @IBOutlet weak var ProbablityLabel: UILabel!
+    @IBOutlet weak var PriceLabel: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
